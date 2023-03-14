cd671e744e58eb1dafface5a2e67f9402f5851e0
Lovely Kate <enbogokina@mail.ru>
2023-03-14 21:43:16 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// 
+string[] arrayInitial = new string[4] {"do", "what", "you", "like"};
+	string[] array = new string[arrayInitial.Length];
+	void threeSymbolArray (string[] array1, string[] array2)
+	{
+	    int count = 0;
+	    for (int i = 0; i < arrayInitial.Length; i++)
+	    {
+	    if(arrayInitial[i].Length <= 3)
+	        {
+	        array[count] = arrayInitial[i];
+	        count++;
+	        }
+	    }
+	}
+	void PrintArray(string[] array)
+	{
+	    for (int i = 0; i < array.Length; i++)
+	    {
+	        Console.Write($"{array[i]} ");
+	    }
+	    Console.WriteLine();
+	}
+	threeSymbolArray(arrayInitial, array);
+	PrintArray(array);
